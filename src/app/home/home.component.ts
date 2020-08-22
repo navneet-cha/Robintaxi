3f3dbf68a18675f3905dd829b28174a654d253b3
DESKTOP-K83H5TF\hp <navneetc185@gmail.com>
2020-08-22 17:46:50 +0530

    robintaxiwebsite

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var openNav;
+declare var closeNav;
 @Component({
-  selector: 'app-home',
+  selector: 'app-home', 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
+  CallopenNav(){
+    openNav();
+  }
+  CallcloseNav(){
+    closeNav();
+  }
   constructor() { }
 
   ngOnInit(): void {
